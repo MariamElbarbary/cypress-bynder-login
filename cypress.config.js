d3923fc6e9b4a01b66c7c7850189ccdd59a58bfd
Mariam Elbarbary <mariamelbarbary@sydl1500792.oc.rabonet.com>
2022-12-13 02:44:44 +0100

    update

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,7 @@ module.exports = defineConfig({
     },
     baseUrl: 'https://wave-trial.getbynder.com/',
     supportFile: "cypress/support/e2e.js",
+    video:false
     env: {
       username: "mariam.elbarbary@rabobank.nl",
       password: "Wa7shteny@11"
