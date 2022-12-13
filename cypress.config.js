8c224d209b970fab1ec79e40b0441a98d46323cf
Mariam Elbarbary <mariamelbarbary@sydl1500792.oc.rabonet.com>
2022-12-13 02:31:36 +0100

    first version of the ci file modified

--- cypress.config.js
+++ cypress.config.js
@@ -9,6 +9,7 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     baseUrl: 'https://wave-trial.getbynder.com/',
+    video: false,
     supportFile: "cypress/support/e2e.js",
     env: {
       username: "mariam.elbarbary@rabobank.nl",
